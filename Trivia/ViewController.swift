26e8ff971e4ef2e29fc7cd010e170300de03102b
Sanaa Bhorkar <sanaa.bhorkar@gmail.com>
2019-07-18 13:32:32 -0700

    Overall design done with 5 riddles

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -10,18 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var questions = ["What has holes but can hold water?", "What has 3 feet but no toes?", "What is young when it is tall and old when it is short?"]
-    var questionAnswers = ["A sponge", "A yardstick", "A candle"]
+    var questions = ["What has holes but can hold water?", "What has 3 feet but no toes?", "What is young when it is tall and old when it is short?", "What kind of room has no door or windows?", "What kind of tree can you carry in your hand"]
     var currentQuestion = 0
+    var questionAnswers = ["a sponge", "a yardstick", "a candle", "a mushroom", "a palm"]
 
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var userAnswerTextField: UITextField!
+    @IBOutlet weak var validationLabel: UILabel!
+    @IBOutlet weak var endOfGameMessageLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         questionLabel.text = questions[currentQuestion]
     }
 
-
+    @IBAction func checkButtonClick(_ sender: Any) {
+        if userAnswerTextField.text == "" {
+            validationLabel.text = "Please enter an answer"
+            return
+        }
+        let userAnswer = userAnswerTextField.text
+        let correctAnswer = questionAnswers[currentQuestion]
+        
+        if(userAnswer!.caseInsensitiveCompare(correctAnswer) == .orderedSame) {
+            validationLabel.text = "Correct! Great job."
+        } else {
+            validationLabel.text = "Incorrect. The correct answer is \(correctAnswer)."
+        }
+        currentQuestion += 1
+        if currentQuestion >= questions.count {
+            endOfGameMessageLabel.text = "Well done. Great game!"
+        } else {
+            questionLabel.text = questions[currentQuestion]
+        }
+        userAnswerTextField.text = ""
+        
+    }
 }
-
