124022d33895ed0b509538ac44e5b15d2463fa20
Rekkno <rekkno.inc@gmail.com>
2019-07-18 10:53:43 -0700

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Sanaa Bhorkar on 7/18/19.
+//  Copyright © 2019 Sanaa Bhorkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
