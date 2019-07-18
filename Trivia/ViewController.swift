1edbf078a4ea65152914cf5d76cbf9bf0a9710b4
Rekkno <rekkno.inc@gmail.com>
2019-07-18 11:29:18 -0700

    Basic app icon, code, and one label with 3 questions. 7/18/19

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var questions = ["What has holes but can hold water?", "What has 3 feet but no toes?", "What is young when it is tall and old when it is short?"]
+    var questionAnswers = ["A sponge", "A yardstick", "A candle"]
+    var currentQuestion = 0
 
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        questionLabel.text = questions[currentQuestion]
     }
 
 
